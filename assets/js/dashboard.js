98a99601bb910f14c6f86b78c84694e8c9a1db1c
proko.chou <proko.chou@cloudmosa.com>
2018-07-06 12:18:09 +0800

    Add dashboard function and contributor in index.html.

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,24 +1,14 @@
-      google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawChart);
+console.log('test');
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses', 'Profit'],
-          ['2014', 1000, 400, 200],
-          ['2015', 1170, 460, 250],
-          ['2016', 660, 1120, 300],
-          ['2017', 1030, 540, 350]
-        ]);
 
-        var options = {
-          chart: {
-            title: 'Company Performance',
-            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-          },
-          bars: 'horizontal' // Required for Material Bar Charts.
-        };
+//Get value of project and change the content in chart
+$('#FormControlSelect1').change(function(){
+    var project= $('#FormControlSelect1').val();
+    console.log(project);
 
-        var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+    //ToDo:Change the content in chart
 
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
+
+  }
+);
+
