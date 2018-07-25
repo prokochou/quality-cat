75a1f59df62c5012ef9dda983e81edba88f2cded
proko.chou <proko.chou@cloudmosa.com>
2018-07-25 10:59:31 +0800

    Add test case detail page.

---
+++ assets/js/case.js
@@ -0,0 +1,17 @@
+console.log('test');
+
+
+//Get value of project and change the content in chart
+$('#FormControlSelect1').change(function(){
+    var project= $('#FormControlSelect1').val();
+    console.log(project);
+
+    //ToDo:Change the content in chart
+
+
+  }
+);
+
+
+$("input").attr("readonly", false);
+
