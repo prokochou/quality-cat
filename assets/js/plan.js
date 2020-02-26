59f09c58f2642ec08b21a838e5c3b46697821054
PROKO <proko@PROKOs-MacBook-Pro.local>
2020-02-26 22:30:40 +0000

    Create plan-new.html

--- assets/js/plan.js
+++ assets/js/plan.js
@@ -1,1 +1,13 @@
+$(document).ready(function(){
+  // $("button").click(function(){
+  //   $("#plan-purpose-others-input").fadeIn("slow");
+  // });
 
+  $("#plan-purpose-others").click(function(){
+			if($("#plan-purpose-others-input").css("display")=="none"){
+				$("#plan-purpose-others-input").show();
+			}else{
+				$("#plan-purpose-others-input").hide();
+			}
+		});
+});
