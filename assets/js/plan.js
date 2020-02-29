4f101c06e91ce2bf637fc4b6515ae33140d9478c
PROKO <proko@PROKOs-MacBook-Pro.local>
2020-02-29 21:01:47 +0000

    Create plan-query.html and fix some bugs.

--- assets/js/plan.js
+++ assets/js/plan.js
@@ -1,14 +1,16 @@
+// plan.html
 function addPlan(){
 	var url = "/quality-cat/plan-new.html";
 	window.location.assign(url);
 }
 
-
+// plan-new.html
 function returnPlan(){
-	var url = "/Users/proko/Documents/quality-cat/plan.html";
+	var url = "/quality-cat/plan.html";
 	window.location.assign(url);
 }
 
+// plan-new.html search others function
 $(document).ready(function(){
   $("#plan-purpose-others").click(function(){
 			if($("#plan-purpose-others-input").css("display")=="none"){
@@ -17,4 +19,14 @@ $(document).ready(function(){
 				$("#plan-purpose-others-input").hide();
 			}
 		});
+});
+
+// plan-query.html
+// Hover on the tag area to show more
+// $(document).ready(function(){
+//     $( "#tag-hover" ).hover(function() {
+//            $('#tag-hover-modal').modal({
+//         show: true
+//     });
+//   });  
 });
